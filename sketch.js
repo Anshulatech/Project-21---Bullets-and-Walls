58890f07bac3be4a95a9c7fb4e95a6e43638eddb
Anshulatech <73025709+Anshulatech@users.noreply.github.com>
2020-10-17 09:10:49 -0400

    Add files via upload
    Added files from the "Project 21" folder

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var bullet, wall;
+var speed, weight;
+var thickness;
+
+function setup() {
+  //step 1
+  createCanvas(1600, 400);
+  
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+
+  wall=createSprite(1200, 200, thickness, height/2);
+  bullet=createSprite(50, 200, 50, 50);
+
+}
+
+function draw() {
+  background('black');  
+ 
+  wall.shapeColor=color(80,80,80);
+
+  bullet.velocityX = speed;
+
+  if(hasCollided(bullet, wall)){
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed * speed/22509;
+    
+    if(deformation>180){
+      bullet.shapeColor=color(255,0,0);
+    }
+   
+    if(deformation<180 && deformation>100){
+      bullet.shapeColor=color(230,230,0);
+    }
+   
+    if(deformation<100){
+      bullet.shapeColor=color(0,255,0);
+    }
+  }
+ 
+  drawSprites();
+}
+
+function hasCollided(lbullet, lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
